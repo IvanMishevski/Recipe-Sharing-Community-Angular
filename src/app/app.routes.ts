a05f2ae1386c94edd25925352c02d66db416b073
Ivan <ivmishevski@gmail.com>
2024-12-01 12:08:40 +0200

    Added login,register and profile pages and navigation for them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'/home', pathMatch: 'full'},
     {path:'home', component:HomeComponent},
     //user routing
-    // {path:'login',component:LoginComponent},
-    // {path:'register',component:RegisterComponent},
-    // {path:'profile',component:ProfileComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'profile',component:ProfileComponent},
     // {path:'logout',component:ProfileComponent},
     //start theme routing
     // {path:'themes', children:[
@@ -17,7 +20,7 @@ export const routes: Routes = [
     // ]},
     // {path:'add-theme',component:AddThemeComponent},
     // //end theme routing
-    // {path:'404*', component:ErrorComponent},
+    {path:'404*', component:ErrorComponent},
     {path:'**', component:ErrorComponent}
 
 
