cfc2d507591e2eee8bc8433392d896b9c269d277
Ivan <ivmishevski@gmail.com>
2024-12-08 10:18:07 +0200

    Added Forms Module to login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login(form: NgForm) {
 
+  }
 }
