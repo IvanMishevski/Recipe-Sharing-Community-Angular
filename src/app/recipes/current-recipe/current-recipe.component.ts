940bc6b280dc0cc424dba93210419104d078b0cc
Ivan <ivmishevski@gmail.com>
2024-12-09 21:19:46 +0200

    Added current-recipe component

---
+++ src/app/recipes/current-recipe/current-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './current-recipe.component.html',
+  styleUrl: './current-recipe.component.css'
+})
+export class CurrentRecipeComponent {
+
+}
