43f5fc1c2180aef64901e6eb38851be3195284a5
Ivan <ivmishevski@gmail.com>
2024-12-09 22:49:51 +0200

    Added edit and delete for owner of a recipe in recipes-catalog

--- src/app/recipes/recipes-catalog/recipes-catalog.component.ts
+++ src/app/recipes/recipes-catalog/recipes-catalog.component.ts
@@ -2,22 +2,29 @@ import { Component } from '@angular/core';
 import { Recipe } from '../../types/recipe';
 import { ApiService } from '../../api.service';
 import { SlicePipe } from '../../shared/pipes/slice.pipe';
+import { RouterLink } from '@angular/router';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-recipes-catalog',
   standalone: true,
-  imports: [SlicePipe],
+  imports: [SlicePipe, RouterLink],
   templateUrl: './recipes-catalog.component.html',
   styleUrl: './recipes-catalog.component.css'
 })
 export class RecipesCatalogComponent {
   recipes: Recipe[] = [];
   isLoading = true;
+  userId = '';
 
-  constructor(private apiService:ApiService){}
+  constructor(private apiService: ApiService, private userService: UserService) { }
 
-  ngOnInit(){
-    this.apiService.getRecipes().subscribe(recipes=>{
+  ngOnInit() {
+    this.userService.getProfile().subscribe((user)=>{
+      console.log(user._id);
+      this.userId = user._id;   
+    })
+    this.apiService.getRecipes().subscribe(recipes => {
       this.recipes = recipes;
       this.isLoading = false;
     });
